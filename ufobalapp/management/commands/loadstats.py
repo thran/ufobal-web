2b5221db7091d9604e59956e07d2e421cab542f8
Jonas Holcner <jojkos@gmail.com>
2015-09-30 10:01:07 +0200

    formating

--- ufobalapp/management/commands/loadstats.py
+++ ufobalapp/management/commands/loadstats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-#python3
+# python3
 
 from django.core.management.base import BaseCommand, CommandError
 # from django.templatetags.static import static
@@ -10,12 +10,12 @@ import os
 from collections import Counter, defaultdict
 from datetime import datetime
 
+
 class Command(BaseCommand):
     help = 'nahrava do databaze ufobalove staty z json ziskaneho prikazem parsestats NEJDRIV BRNO STRELCI, PAK ASISTENCE A PAK NIZKOV to stejny'
 
     def add_arguments(self, parser):
         parser.add_argument('statsfile', nargs=1, type=str, help='ufobalove staty v json')
-
 
     def handle(self, *args, **options):
         shooters = False
@@ -38,22 +38,20 @@ class Command(BaseCommand):
                 nizkov = True
 
             if shooters and brno:
-                #testovaci mazani vseho pokazde
+                # testovaci mazani vseho pokazde
                 Player.objects.all().delete()
                 Team.objects.all().delete()
                 Goal.objects.all().delete()
                 Tournament.objects.all().delete()
-
-
 
             with open(os.path.join('data_source', filename), 'r') as statsfile:
                 stats = json.load(statsfile)
                 name = filename.split('-')[0]
                 for datestring in stats['dates']:
                     date = datetime.strptime(datestring, '%d.%m.%Y').date()
-                    #created==False znamena ze uz byl driv vytvoren
+                    # created==False znamena ze uz byl driv vytvoren
                     tournament, created = Tournament.objects.get_or_create(name=name, date=date)
-                    #fake zapas pro vsechny strely/asistence na turnaji
+                    # fake zapas pro vsechny strely/asistence na turnaji
                     match, created = Match.objects.get_or_create(tournament=tournament, fake=True)
 
                 namecounter = {}
@@ -62,30 +60,31 @@ class Command(BaseCommand):
                     namecounter[player['name']] += 1
 
                 for player in stats['players']:
-                    #vytvoreni hrace
+                    # vytvoreni hrace
                     playerobj, created = Player.objects.get_or_create(nickname=player['name'])
 
-                    #pokud existuje hrac se stejnym jmenem
-                    if not created and shooters and brno: #u strelcu se vytvari ruzni hraci, u asistenci uz se to jen doplnuje
-                        playerobj, created = Player.objects.get_or_create(nickname=player['name']+"-"+player['team'])
+                    # pokud existuje hrac se stejnym jmenem
+                    if not created and shooters and brno:  # u strelcu se vytvari ruzni hraci, u asistenci uz se to jen doplnuje
+                        playerobj, created = Player.objects.get_or_create(
+                            nickname=player['name'] + "-" + player['team'])
                     elif not created and nizkov and shooters:
-                        #pokud je jen jednou, priradi se uz k existujicimu brnenskemu, jinak bude nove jmeno
+                        # pokud je jen jednou, priradi se uz k existujicimu brnenskemu, jinak bude nove jmeno
                         if namecounter[player['name']] > 1:
-                            playerobj, created = Player.objects.get_or_create(nickname=player['name']+"-"+player['team'])
-
+                            playerobj, created = Player.objects.get_or_create(
+                                nickname=player['name'] + "-" + player['team'])
 
                     if 'gender' in player:
                         playerobj.gender = player['gender']
                     playerobj.save()
 
-                    #vytvoreni golu/asistenci hrace
+                    # vytvoreni golu/asistenci hrace
                     for info in player:
                         if info not in ['name', 'team', 'gender']:
                             date = datetime.strptime(info, '%d.%m.%Y').date()
                             tournament = Tournament.objects.get(date=date)
                             match = Match.objects.get(tournament=tournament)
 
-                            #pokud ma nejake staty
+                            # pokud ma nejake staty
                             if player[info] and player[info] not in ['-', '?']:
                                 shotscount = int(player[info])
                                 for i in range(shotscount):
@@ -97,7 +96,7 @@ class Command(BaseCommand):
                                     newgoal.save()
 
 
-                            #vytvoreni a prirazeni tymu ve kterych hrac byl v rocnicich kdy hral
+                                    # vytvoreni a prirazeni tymu ve kterych hrac byl v rocnicich kdy hral
                                 teams = player['team'].split(',')
                                 if len(teams) == 1 and shotscount > 0:
                                     teamname = player['team']
@@ -107,6 +106,3 @@ class Command(BaseCommand):
                                         team=teamobj, tournament=tournament)
                                     teamtourobj.players.add(playerobj)
                                     teamtourobj.save()
-
-
-
