616923446918d941e4314d10c5297e28ccecfe3f
Thran <exthran@gmail.com>
2015-10-02 18:55:43 +0200

    formating

--- managestats/views.py
+++ managestats/views.py
@@ -8,38 +8,42 @@ from django.db.models import Q
 
 from ufobalapp.models import Player, Tournament, Team, TeamOnTournament
 
+
 def index(request):
     return render(request, 'index.html')
 
 
 def players(request):
-    players = Player.objects.order_by('nickname').all()
+    player_list = Player.objects.order_by('nickname').all()
 
-    context = {'players': players}
+    context = {'players': player_list}
     return render(request, 'players.html', context)
 
-def tournaments(request):
-    tournaments = Tournament.objects.order_by('date').all()
 
-    context = {'tournaments':tournaments}
+def tournaments(request):
+    tournament_list = Tournament.objects.order_by('date').all()
+
+    context = {'tournaments': tournament_list}
     return render(request, 'tournaments.html', context)
+
 
 def tournament(request, tournament_id):
     tournament = get_object_or_404(Tournament.objects.order_by('date'), id=tournament_id)
     teams = TeamOnTournament.objects.order_by('name').filter(tournament=tournament)
 
-    knowns = Player.objects.filter(teams__tournament = tournament)
+    knowns = Player.objects.filter(teams__tournament=tournament)
     unknowns = Player.objects.order_by('nickname').filter(
         Q(goals__match__tournament=tournament) | Q(assistances__match__tournament=tournament)
     ).distinct().exclude(id__in = knowns)
 
-    context = {'tournament':tournament,
-               'teams':teams,
-               'unknowns':unknowns}
+    context = {'tournament': tournament,
+               'teams': teams,
+               'unknowns': unknowns}
     return render(request, 'tournament.html', context)
 
-def teams(request):
-    teams = Team.objects.order_by('name')
 
-    context = {'teams':teams}
+def teams(request):
+    teams_list = Team.objects.order_by('name')
+
+    context = {'teams': teams_list}
     return render(request, 'teams.html', context)
