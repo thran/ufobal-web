7e10c1e1a997f25d1894c26c0aed0f95464548ef
Thran <exthran@gmail.com>
2015-10-04 10:52:54 +0200

    add missing static roots

--- ufobal/settings.py
+++ ufobal/settings.py
@@ -85,9 +85,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_URL = '/static/'
