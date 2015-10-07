65aad78ee0e4b566ab56c6d19bb8efb49313c632
Thran <exthran@gmail.com>
2015-10-08 00:52:55 +0200

    redone bulk edit of players

--- managestats/static/managestats/js/app.js
+++ managestats/static/managestats/js/app.js
@@ -18,6 +18,71 @@ app.controller("player-detail", ["$scope", "$http", "$location", 'djangoUrl',
     };
 
     get_player();
+}]);
 
-    $scope.test = 42;
+app.directive('datepicker', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function ($scope, element, attrs, ngModelCtrl) {
+            $(element).datepicker({
+                dateFormat: 'yy-mm-dd',
+                changeMonth: true,
+                changeYear: true,
+                yearRange: "1950:2015",
+                onSelect: function (date) {
+                    ngModelCtrl.$setViewValue(date);
+                    $scope.$apply();
+                }
+            });
+        }
+    };
+});
+
+
+app.controller("player-bulk-edit", ["$scope", "$http", 'djangoUrl', function ($scope, $http, djangoUrl) {
+
+
+    $scope.get_players = function(){
+        $http.get(djangoUrl.reverse("api:get_players"))
+            .success(function(response){
+                $scope.players = response;
+            });
+    };
+
+    $scope.genders = [
+        {id: 'man', text: "muž"},
+        {id: 'woman', text: "žena"}
+    ];
+
+    $scope.$watch("players", function(n, o){
+        if (!n){
+            return;
+        }
+        $scope.changed = false;
+        $scope.saving = false;
+        angular.forEach(n, function(player, key) {
+            $scope.changed = $scope.changed || player.changed;
+            $scope.saving = $scope.saving || player.saving;
+        });
+    },true);
+
+
+    $scope.save = function(){
+        angular.forEach($scope.players, function(player, key) {
+            if (player.changed){
+                player.saving = true;
+                $http.post(djangoUrl.reverse("api:save_player"), player)
+                    .success(function(response){
+                        player.changed = false;
+                        player.saving = false;
+                    })
+                    .error(function (response) {
+                        player.saving = false;
+                    });
+            }
+        });
+    };
+
+    $scope.get_players();
 }]);
