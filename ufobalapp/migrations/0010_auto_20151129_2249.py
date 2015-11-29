f0a70cad87e110dc1c0af84082b52c92e8c44648
Thran <exthran@gmail.com>
2015-11-29 22:52:01 +0100

    allow empty players field

---
+++ ufobalapp/migrations/0010_auto_20151129_2249.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ufobalapp', '0009_auto_20151124_2204'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='goal',
+            name='shooter',
+            field=models.ForeignKey(related_name='goals', verbose_name='střelec', to='ufobalapp.Player', null=True),
+        ),
+        migrations.AlterField(
+            model_name='teamontournament',
+            name='players',
+            field=models.ManyToManyField(related_name='tournaments', blank=True, verbose_name='Hráči', to='ufobalapp.Player'),
+        ),
+    ]
