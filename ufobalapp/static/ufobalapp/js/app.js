68c784c5c37017d1ed9b7b508bcd805088ab3ba7
Thran <exthran@gmail.com>
2015-12-04 22:28:36 +0100

    finish (lot of work) players section

--- ufobalapp/static/ufobalapp/js/app.js
+++ ufobalapp/static/ufobalapp/js/app.js
@@ -15,6 +15,20 @@ app.run(["$rootScope", "$location", function ($rootScope, $location) {
     });
 }]);
 
+app.run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
+    var original = $location.path;
+    $location.path = function (path, reload) {
+        if (reload === false) {
+            var lastRoute = $route.current;
+            var un = $rootScope.$on('$locationChangeSuccess', function () {
+                $route.current = lastRoute;
+                un();
+            });
+        }
+        return original.apply($location, [path]);
+    };
+}]);
+
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $locationProvider.hashPrefix('!');
         $routeProvider.
@@ -23,6 +37,10 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
                 controller: "home"
             }).
             when('/hraci', {
+                templateUrl: 'players.html',
+                controller: "players"
+            }).
+            when('/hraci/:id/:hrac', {
                 templateUrl: 'players.html',
                 controller: "players"
             }).
@@ -39,13 +57,64 @@ app.controller("home", ["$scope", function ($scope) {
 }]);
 
 
+app.controller("players", ["$scope", "dataService", "$location", "$routeParams", function ($scope, dataService, $location, $routeParams) {
+    var id = parseInt($routeParams.id);
+    $scope.genders = genders;
 
-app.controller("players", ["$scope", "dataService", function ($scope, dataService) {
-    dataService.getPlayers().then(function(players){
-        $scope.players = players;
-    });
+    $scope.computeAge = function(){
+        if ($scope.player.birthdate){
+            var ageDifMs = Date.now() - Date.parse($scope.player.birthdate);
+            var ageDate = new Date(ageDifMs);
+            $scope.player.age = Math.abs(ageDate.getUTCFullYear() - 1970);
+        }
+    };
 
     $scope.selectPlayer = function(player){
         $scope.player = player;
+        $location.path("/hraci/" + player.pk + "/" + player.nickname, false);
     };
+
+    $scope.closePlayer = function(){
+        $scope.player = null;
+        $location.path("/hraci", false);
+    };
+
+    $scope.addAttendance = function(){
+        dataService.addAttendance($scope.player, $scope.selectedTeam);
+    };
+
+    $scope.removeAttendance = function(team){
+        if (confirm("Opravdu chcete smazat tuto účast na turnaji?")) {
+            dataService.removeAttendance($scope.player, team);
+        }
+    };
+
+    $scope.save = function(){
+        dataService.savePlayer($scope.player)
+            .success(function(){
+                $scope.edit = false;
+            });
+    };
+
+    dataService.getPlayers().then(function(players){
+        $scope.players = players;
+        if (id) {
+            $scope.selectPlayer(dataService.getObject("players", id));
+        }
+    });
+
+    dataService.getTournaments().then(function(tournaments){
+        $scope.tournaments = tournaments;
+    });
 }]);
+
+
+var shallow_copy = function(obj){
+    var newObj = {};
+    angular.forEach(obj, function(value, key){
+        if (typeof value !== "object"){
+            newObj[key] = value;
+        }
+    });
+    return newObj;
+};
