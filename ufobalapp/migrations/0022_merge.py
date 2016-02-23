0942cf85d63175ab270e0e915f2402a0e0758173
Thran <thran@centrum.cz>
2016-02-23 22:59:47 +0100

    add migration marge

---
+++ ufobalapp/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ufobalapp', '0021_player_pairing_token'),
+        ('ufobalapp', '0021_tournament_field_count'),
+    ]
+
+    operations = [
+    ]
