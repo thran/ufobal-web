e074a8683c4ddf1f5669c7507810becfb5c74098
Thran <thran@centrum.cz>
2016-02-24 12:22:45 +0100

    migration merge

---
+++ ufobalapp/migrations/0024_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ufobalapp', '0023_auto_20160224_1038'),
+        ('ufobalapp', '0023_auto_20160224_1110'),
+    ]
+
+    operations = [
+    ]
