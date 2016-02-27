822b3dc81bbee630bb304d482b80042241030d84
Thran <exthran@gmail.com>
2016-02-27 18:24:05 +0100

    script for deleting duplicates

---
+++ ufobalapp/management/commands/delete_duplicates.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+from ufobalapp.models import Shot, Goal
+
+
+class Command(BaseCommand):
+    help = 'delete duplicates'
+
+    def handle(self, *args, **options):
+        for shot in Shot.objects.all():
+            if Shot.objects.filter(match=shot.match, time=shot.time, team=shot.team).count() > 1:
+                print(Shot.objects.filter(match=shot.match, time=shot.time, team=shot.team).values_list("pk"))
+                shot.delete()
+
+        for goal in Goal.objects.filter(match__fake=False):
+            if Goal.objects.filter(match=goal.match, time=goal.time, shooter=goal.shooter, assistance=goal.assistance).count() > 1:
+                print(Goal.objects.filter(match=goal.match, time=goal.time, shooter=goal.shooter, assistance=goal.assistance))
+                shot.delete()
+
