319b63c66ad21e54386c59325a44e8d579364bbf
Jiří Thran Řihák <exthran@gmail.com>
2022-03-19 08:40:12 +0100

    add missing default goalie in admin

--- ufobalapp/admin.py
+++ ufobalapp/admin.py
@@ -106,7 +106,7 @@ class TeamAdmin(admin.ModelAdmin):
 
 class TeamTournamentAdmin(admin.ModelAdmin):
     list_display = ('team', 'name', 'tournament_name', 'tournament_date', 'rank')
-    fields = [('team', 'registration_time_with_seconds'), 'captain', 'tournament', 'name', 'name_short', 'rank', ('contact_mail', 'contact_phone'), 'strength']
+    fields = [('team', 'registration_time_with_seconds'), 'captain', 'default_goalie', 'tournament', 'name', 'name_short', 'rank', ('contact_mail', 'contact_phone'), 'strength']
     readonly_fields = ['registration_time_with_seconds']
 
     search_fields = ['name', 'team__name', 'tournament__name', 'tournament__date']
